e31c529704a63d098b35ec8c72dff33308caa377
Jonathan King <jonpking@gmail.com>
2020-07-13 16:13:34 -0700

    added NEW route and CREATE route

--- app.js
+++ app.js
@@ -37,12 +37,29 @@ app.get("/", function (req, res) {
     res.redirect("/blogs");
 })
 
+// INDEX ROUTE
 app.get("/blogs", function (req, res) {
     Blog.find({}, function (err, blogs) {
         if (err) {
             console.log("ERROR!");
         } else {
             res.render("index", { blogs: blogs });
+        }
+    });
+});
+
+// NEW ROUTE
+app.get("/blogs/new", function (req, res) {
+    res.render("new");
+});
+
+// CREATE ROUTE
+app.post("/blogs", function (req, res) {
+    Blog.create(req.body.blog, function (err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
         }
     });
 });
