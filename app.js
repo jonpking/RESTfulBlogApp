b1ad141a56c677c48e2b622754a505c8f808c372
Jonathan King <jonpking@gmail.com>
2020-07-13 15:26:24 -0700

    connected db, set up initial blogSchema, added test post, created basic index and / routes

--- app.js
+++ app.js
@@ -1,5 +1,52 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Connected to DB!"))
+    .catch(err => console.log(error.message));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1542549237432-a176cb9d5e5e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=651&q=80",
+//     body: "HELLO THIS IS A BLOG POST!"
+// });
+
+// RESTFUL ROUTES
+
+app.get("/", function (req, res) {
+    res.redirect("/blogs");
+})
+
+app.get("/blogs", function (req, res) {
+    Blog.find({}, function (err, blogs) {
+        if (err) {
+            console.log("ERROR!");
+        } else {
+            res.render("index", { blogs: blogs });
+        }
+    });
+});
+
+app.listen(3000, function () {
+    console.log("SERVER IS RUNNING!")
+});
