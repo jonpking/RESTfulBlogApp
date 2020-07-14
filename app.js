5e8c658d59238cc72ba66d38fd2f6d0390e2ffdc
Jonathan King <jonpking@gmail.com>
2020-07-13 18:31:20 -0700

    added method-override, added edit and update routes

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
 })
     .then(() => console.log("Connected to DB!"))
     .catch(err => console.log(error.message));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
 const blogSchema = new mongoose.Schema({
@@ -71,6 +73,28 @@ app.get("/blogs/:id", function (req, res) {
             res.redirect("/blogs");
         } else {
             res.render("show", { blog: foundBlog });
+        }
+    });
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function (req, res) {
+    Blog.findById(req.params.id, function (err, foundBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", { blog: foundBlog });
+        }
+    });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function (req, res) {
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
+        if (err) {
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id)
         }
     });
 });
